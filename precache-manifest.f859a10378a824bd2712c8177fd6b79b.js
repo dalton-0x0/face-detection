8494858b0f8ca48bb344b7e9bdacbe76fce3903c
Dalton Okechukwu <chike911@yahoo.com>
2019-11-11 17:38:25 -0700

    Updates

---
+++ precache-manifest.f859a10378a824bd2712c8177fd6b79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0c98f8cec04c1f150eb9696936ee09",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "6dd379cf29a9e70e6faf",
+    "url": "/face-detection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "25818cb627cdbc51bf08",
+    "url": "/face-detection/static/css/main.4ec929cc.chunk.css"
+  },
+  {
+    "revision": "6dd379cf29a9e70e6faf",
+    "url": "/face-detection/static/js/2.52ca4686.chunk.js"
+  },
+  {
+    "revision": "25818cb627cdbc51bf08",
+    "url": "/face-detection/static/js/main.5c6ac076.chunk.js"
+  },
+  {
+    "revision": "3bf4aa970d8dffce08ce",
+    "url": "/face-detection/static/js/runtime-main.079a0bb1.js"
+  }
+]);
