c31a711ddbd8900ecc69c5d5661998b94869b599
Dalton Okechukwu <chike911@yahoo.com>
2019-11-19 00:32:00 -0700

    rank receiving props for dynamic display on login

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,11 +1,13 @@
 import React from "react";
 const Fragment = React.Fragment;
 
-export const Rank = () => {
-	return (
-		<Fragment>
-			<div className="f2 white">{`Hello! Your current rank is:`}</div>
-			<div className="f1 white">{`#5`}</div>
-		</Fragment>
-	);
+const Rank = ({ name, entries }) => {
+  return (
+    <Fragment>
+      <div className="f2 white">{`Hello ${name}! Your current rank is:`}</div>
+      <div className="f1 white">{`${entries}`}</div>
+    </Fragment>
+  );
 };
+
+export default Rank;
