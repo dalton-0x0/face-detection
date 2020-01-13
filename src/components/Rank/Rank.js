921edfcd50402ec4b8d3b3fffae3080599e13e4d
Dalton Okechukwu <chike911@yahoo.com>
2020-01-13 13:43:52 -0700

    refactored rank component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Fragment = React.Fragment;
 const Rank = ({ name, entries }) => {
   return (
     <Fragment>
-      <div className="f2 white">{`Hello ${name}! Your current rank is:`}</div>
+      <div className="f2 white">{`Hello ${name}! Your current image entry count is:`}</div>
       <div className="f1 white">{`${entries}`}</div>
     </Fragment>
   );
